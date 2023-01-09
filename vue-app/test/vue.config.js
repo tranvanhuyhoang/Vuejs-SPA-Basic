cb6a9144a6dc7a96e4f479a1330127bf5db666aa
hoangtvh <hoangtvh@vng.com.vn>
2023-01-09 13:27:52 +0700

    update

--- vue-app/test/vue.config.js
+++ vue-app/test/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  output: {
-    libraryTarget: 'system'
-  },
+  transpileDependencies: true
 })
