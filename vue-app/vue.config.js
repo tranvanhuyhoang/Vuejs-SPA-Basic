5d3c2992ae22961dd40a1f3fe14c18e0441a1931
hoangtvh <tranvanhuyhoang@gmail.com>
2023-01-11 20:58:44 +0700

    fix: cannot read properties of undefined (reading 'meta') in Vue 3 single-spa app

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    output: {
+      libraryTarget: "system",
+    },
+  }
+}
